cf0357d89c651b0118772e40a870efbc2944e31b
YN0220SV <YN0220SV@gmail.com>
2022-03-19 10:21:33 +0900

    addEasy11

--- Easy/Easy11.cpp
+++ Easy/Easy11.cpp
@@ -1,3 +1,4 @@
+// https://atcoder.jp/contests/abc068/tasks/abc068_b
 #include <bits/stdc++.h>
 using namespace std;
 int main()
@@ -5,5 +6,16 @@ int main()
     int n = 0;
     cin >> n;
     vector<int> k{1, 2, 4, 8, 16, 32, 64};
-    
+
+    for (int i = 0; i < 7; i++)
+    {
+        if (k[i] > n)
+        {
+            cout << k[i - 1] << endl;
+            ;
+            return 0;
+        }
+    }
+    cout << 64 << endl;
+    return 0;
 }
